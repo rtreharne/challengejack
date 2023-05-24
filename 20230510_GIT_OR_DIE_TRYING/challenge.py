771031e5c516b41977a146a0cd705c0fdb747ca8
Treharne <R.Treharne@liverpool.ac.uk>
2023-05-24 13:53:41 +0100

    updated the files

--- 20230510_GIT_OR_DIE_TRYING/challenge.py
+++ 20230510_GIT_OR_DIE_TRYING/challenge.py
@@ -1,5 +1,31 @@
-"""
-Read in the data in the 'data.csv' file.
-Create a scatterplot using matplotlib of x vs y.
-Save the figure as 'output.jpg'
-"""
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+df.head()
+
+x = df['x']
+y = df ['y']
+
+from matplotlib import pyplot as plt
+
+import numpy as py 
+
+
+# extract the x and y data from the DataFrame
+x = df['x'].to_numpy()
+y = df['y'].to_numpy()
+
+# add a new dimension to the x and y arrays
+x = x[:, None]
+y = y[:, None]
+
+# create a scatter plot of x vs y
+plt.scatter(x, y)
+
+# set the axis labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('X vs Y')
+
+# show the plot
+plt.show()
